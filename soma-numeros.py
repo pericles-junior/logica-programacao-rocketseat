d0c390460a48a9b73d9c693ad313d1d65b0a6384
Péricles Jr <periclesmjr@hotmail.com>
2025-03-26 07:54:50 -0300

    repetindo tarefas com looping (while/for)

---
+++ soma-numeros.py
@@ -0,0 +1,13 @@
+soma = 0
+n = 1
+
+# while n <= 10:
+#     soma += n
+#     n += 1
+
+for index in range(1,11):
+    soma += index
+
+# soma = sum([i for i in range(1,11)])
+
+print(f'A soma dos nossos números de 1 a 10 é: {soma}')
