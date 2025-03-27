1958eb3c3a57e3869adbce981be9a907f9fab55a
Péricles Júnior <periclesmjr@hotmail.com>
2025-03-27 19:38:35 -0300

    escopo global e local

--- funcoes.py
+++ funcoes.py
@@ -2,3 +2,15 @@ def olaPessoa(nome):
   print(f'Olá, {nome}!')
 
 olaPessoa(input('Digite seu nome: '))
+
+def multiplicaDoisNumeros(a, b = 2):
+  """ Multiplica dois números """
+  return a * b
+
+x = 5 # Variável global
+def soma():
+  x = 10 # Variável local
+  print(x)
+  return x + 1
+soma()
+print(x)
